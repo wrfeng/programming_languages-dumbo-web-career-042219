b9ceeca4b52dd652b65d63355f96c77e69eb3bd0
wei feng <theweifeng@gmail.com>
2019-04-17 23:23:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |paradigm, language|
     language.each do |programming_language, style|
       style.each do |type, language_type|
-        hash[programming_language] = {}
+        hash[programming_language] = {} if hash[programming_language] == nil
         hash[programming_language][:type] = language_type
         
         if hash[programming_language][:style] == nil
