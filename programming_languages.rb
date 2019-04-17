8cad1437a1a767f866cc284916751531ba6790dc
wei feng <theweifeng@gmail.com>
2019-04-17 23:18:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,10 +2,17 @@ def reformat_languages(languages)
   hash = {}
   
   languages.each do |paradigm, language|
-    language.each do |type, language_type|
-      hash[language] = {}
-      hash[language][type] = language_type
-      # hash[language][:stlye] << paradigm
+    language.each do |programming_language, style|
+      style.each do |type, language_type|
+        hash[programming_language] = {}
+        hash[programming_language][:type] = language_type
+        
+        if hash[programming_language][:style] == nil
+          hash[programming_language][:style] = [style]
+        else
+          hash[programming_language][:style] << style]
+        end
+      end
     end
   end
   
