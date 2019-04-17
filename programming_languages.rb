4ef9ee35a056b9b425d5873511baecc29fcaf577
wei feng <theweifeng@gmail.com>
2019-04-17 22:58:31 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  
+  languages.each do |paradigm, language|
+    language.each do |type, language_type|
+      hash[language] = {}
+      hash[language][:new_type] = type
+      hash[language][:stlye] << paradigm
+    end
+  end
+  
+  hash
 end
